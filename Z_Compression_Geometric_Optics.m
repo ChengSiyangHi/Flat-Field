b533eab0542d7406cf5d7653393233234cff6ea0
Siyang Cheng <143525650+ChengSiyangHi@users.noreply.github.com>
2024-02-02 16:19:59 -0600

    Add files via upload

--- Z_Compression_Geometric_Optics.m
+++ Z_Compression_Geometric_Optics.m
@@ -1,12 +1,13 @@
 %% Z_Compression_Geometric_Optics
 % Reference: S.V. LOGINOV et al. BioRxiv (2024)
 % Last edited by Siyang Cheng 2024/02/02
+clc
 
 n1 = 1.52; % Refractive index of the oil
 n2 = 1.34; % Refractive index of the sample
 NA = 1.40; % Numerical aperture of the objective
 
-Za = 10; % The actual z position of interest from the coverslip (um)
+Za = 4; % The actual z position of interest from the coverslip (um)
 lambda = 560E-3;
 
 m = sqrt(n2^2-n1^2);
@@ -17,7 +18,7 @@ else
     eps = -lambda/4/Za/n2;
 end
 
-zeta_univ = n2/n1*(1-eps+m*n1*sqrt(eps*(2-eps)))/(1-(n2/n1)^2*eps*(2-eps));
+zeta_univ = n2/n1*(1-eps+m/n1*sqrt(eps*(2-eps)))/(1-(n2/n1)^2*eps*(2-eps));
 zeta_crit = real((n1-sqrt(n1^2-NA^2)/(n2-sqrt(n2^2-NA^2))));
 
 if n2>n1 || n2==n1
